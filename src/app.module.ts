055c24d923a0272c74ecc9ecbcfe6dfa437aefb1
truongtrong99 <truongtrong400@gmail.com>
2025-09-04 16:05:47 +0700

    #20 -Implement user module with CRUD operations and DTOs

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
 @Module({
   imports: [
     MongooseModule.forRootAsync({
@@ -16,7 +17,8 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
       {
         isGlobal: true,
       }
-    )
+    ),
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
