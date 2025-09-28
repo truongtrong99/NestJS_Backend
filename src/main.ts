51496fd2069d0ce3d57f315a6b030a1f1e1dd308
truongtrong99 <truongtrong400@gmail.com>
2025-09-28 13:01:47 +0700

    #22 - 5. Enhance user DTO validation: add email validation and required checks for email and password fields in CreateUserDto

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 require('dotenv').config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(configService.get<string>('PORT'));
 }
 bootstrap();
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,10 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
 export class CreateUserDto {
+    @IsEmail({}, { message: 'Invalid email format' })
+    @IsNotEmpty({ message: 'Email is required' })
     email: string;
+
+    @IsNotEmpty({ message: 'Password is required' })
     password: string;
     name: string;
     address?: string;
