448bf3ee7c3c32cd044b1d7b5f52e35d8424a2fe
truongtrong99 <truongtrong400@gmail.com>
2025-09-28 12:20:32 +0700

    #22 - 1. Refactor user creation logic: update UsersController and UsersService to accept individual parameters instead of DTO

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './schemas/user.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+
+  constructor(@InjectModel(User.name) private userModel: Model<User>) { }
+
+  async create(email: string, password: string, name: string) {
+    const user = await this.userModel.create({ email, password, name });
+    return user;
   }
 
   findAll() {
