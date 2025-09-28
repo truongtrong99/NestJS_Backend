3e02366a1c11cb76f1b7af72cc1ab59670060094
truongtrong99 <truongtrong400@gmail.com>
2025-09-28 12:32:25 +0700

    #22 - 2.Refactor user service: extract password hashing logic into a separate method and update user creation to use hashed password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,14 +4,22 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './schemas/user.schema';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
+import { genSaltSync, hashSync } from 'bcryptjs';
 
 @Injectable()
 export class UsersService {
 
   constructor(@InjectModel(User.name) private userModel: Model<User>) { }
 
+  hashPassword(password: string): string {
+    const salt = genSaltSync(10);
+    const hash = hashSync(password, salt);
+    return hash;
+  }
+
   async create(email: string, password: string, name: string) {
-    const user = await this.userModel.create({ email, password, name });
+    const hashedPassword = this.hashPassword(password);
+    const user = await this.userModel.create({ email, password: hashedPassword, name });
     return user;
   }
 
