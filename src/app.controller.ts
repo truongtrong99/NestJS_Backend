d4488783a61abe56dffba1f74c0605463a5289fd
truongtrong99 <truongtrong400@gmail.com>
2025-10-01 18:53:34 +0700

    #39 - Implement JWT authentication: add JwtModule and update AuthService for token generation

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,11 @@ import { AppService } from './app.service';
 import { ConfigService } from '@nestjs/config';
 import { AuthGuard } from '@nestjs/passport';
 import { LocalAuthGuard } from './auth/local-auth.guard';
+import { AuthService } from './auth/auth.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService, private configService: ConfigService) { }
+  constructor(private readonly appService: AppService, private configService: ConfigService, private authService: AuthService) { }
 
   @Get()
   getHello(): string {
@@ -16,6 +17,6 @@ export class AppController {
   @UseGuards(LocalAuthGuard)
   @Post('/login')
   handleLogin(@Request() req) {
-    return req.user;
+    return this.authService.login(req.user);
   }
 }
