08301e985c32079a2bfe7dd7cee47eaffe82ce5b
truongtrong99 <truongtrong400@gmail.com>
2025-10-01 19:25:22 +0700

    #41 - Enable authentication globally

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -1,6 +1,15 @@
 
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') { }
+export class JwtAuthGuard extends AuthGuard('jwt') {
+
+    handleRequest(err, user, info) {
+        // You can throw an exception based on either "info" or "err" arguments
+        if (err || !user) {
+            throw err || new UnauthorizedException("Unauthorized access - invalid or expired token");
+        }
+        return user;
+    }
+}
