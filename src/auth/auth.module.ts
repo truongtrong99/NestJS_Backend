6d49f45059f8bbe81ea64cda82712b590eb372b1
truongtrong99 <truongtrong400@gmail.com>
2025-10-02 09:34:53 +0700

    #43 - Add ms package and update JWT secret handling in AuthModule and JwtStrategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,14 +6,15 @@ import { LocalStrategy } from './passport/local.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { JwtStrategy } from './passport/jwt.strategy';
+import ms from 'ms';
 @Module({
   imports: [UsersModule, PassportModule,
     JwtModule.registerAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET'),
+        secret: configService.get<string>('JWT_ACCESS_TOKEN'),
         signOptions: {
-          expiresIn: configService.get<string>('JWT_EXPIRES_IN'),
+          expiresIn: ms(configService.get<string>('JWT_ACCESS_EXPIRES')),
         },
       }),
       inject: [ConfigService],
